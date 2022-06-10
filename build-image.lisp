b7b17f450d0965aa3de4cd9b25039a5060568b3c
vindarel <vindarel@mailz.org>
2022-06-10 17:00:42 +0200

    build-image: use asdf:load-asd

--- build-image.lisp
+++ build-image.lisp
@@ -1,6 +1,10 @@
 
-(load "ciel.asd")
+(in-package :cl-user)
+
+(asdf:load-asd "ciel.asd")
 (ql:quickload "swank")
 (ql:quickload "ciel")
+
 (in-package :ciel-user)
+
 (sb-ext:save-lisp-and-die "ciel")
